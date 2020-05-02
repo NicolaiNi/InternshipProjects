1c8ed49c9045ae3251bba1e6aab6a3017e061ebe
NicolaiNi <61543159+NicolaiNi@users.noreply.github.com>
2020-05-02 21:51:50 +0200

    Add files via upload

---
+++ DiffReporter/Program.cs
@@ -0,0 +1,22 @@
+﻿using DiffReporter.Logic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DiffReporter {
+    class Program {
+        static void Main(string[] args) {
+            ////Get Path of two Folders: EFAM and Manually
+            string rootPath = Directory.GetCurrentDirectory();
+
+            var diffHelper = new DiffHelper(rootPath);
+            diffHelper.Init();
+            diffHelper.Compare();
+            Console.WriteLine($"The application finished successfully. The file was created in {rootPath}.");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
